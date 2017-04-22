dc871b3f5f8b40ecb1f6f3cbcb7479e4ecba3f35
Bryan Falls <bfalls1587@gmail.com>
2017-04-22 15:55:05 -0400

    adjustable interval property

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+// y/x = 0 x  - - - - - - - >
+// y
+// |
+// |
+// |
+// |
+// |
+// |
+// V
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext("2d");
 var x = canvas.width/2;
 var y = canvas.height/2;
 var dx = 2;
 var dy = -2;
+var inter = 1000;
 
 function getArea(r){
 	return Math.pow(r, 2) * Math.PI;
@@ -12,7 +22,7 @@ function getRadius() {
 	return Number(document.getElementById('radius').value);
 }
 
-function drawBall() {
+function drawBall() { //function to draw ball each time setInterval iterates
 	var radius = Number(document.getElementById('radius').value);
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, Math.PI*2);
@@ -20,14 +30,21 @@ function drawBall() {
     ctx.fill();
     ctx.closePath();
     x += dx;
-    y += dy;	
+    y += dy;
+    if(x + dx > canvas.width-radius || x + dx < radius) { // change direction of ball if it reaches sides
+    dx = -dx;
 }
 
-function draw() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	if(y + dy > canvas.height-radius || y + dy < radius) { // change direction of ball if it reaches bottom/top
+	    dy = -dy;
+	}	
+}
+
+function draw() { //function setInterval runs each time.
+	ctx.clearRect(0, 0, canvas.width, canvas.height); //clears canvas to create movement via drawings
     drawBall();
-    x += dx;
-    y += dy;
+    x += dx; // increases x cordinates (gives direction)
+    y += dy; // increases y cordinates (gives direction)
 }
 
 function btnClick() {
@@ -40,6 +57,6 @@ document.getElementById('goBtn').addEventListener('click', function() {
 })
 
 document.getElementById('goBtn').addEventListener('click', function() {
-	setInterval(draw, 10);
+	setInterval(draw, inter);
 });
 
